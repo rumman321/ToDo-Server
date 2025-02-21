b321c862f9c8bff8a093329b7a6c0234d4a2860a
rumman321 <cset-210652@dti.ac>
2025-02-22 02:16:08 +0600

    make delete api for task delete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.ogyvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogyvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +48,12 @@ async function run() {
     })
     app.get('/tasks', async(req,res)=>{
         const result = await taskCollection.find().toArray()
+        res.send(result)
+    })
+    app.delete('/tasks/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = {_id:new ObjectId(id)}
+        const result = await taskCollection.deleteOne(query)
         res.send(result)
     })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
